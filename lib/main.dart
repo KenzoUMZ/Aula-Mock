a2a7d6ec976de38b29a6c836a3a9e334b5419472
KenzoUMZ <kenzoumezawa@gmail.com>
2022-07-10 06:42:12 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:aula_mock/view/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Home());
+
---
+++ lib/view/home.dart
@@ -0,0 +1,50 @@
+import 'package:aula_mock/model/album.dart';
+import 'package:aula_mock/controller/fetchAlbum.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
